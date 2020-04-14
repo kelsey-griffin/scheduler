b7200cd9baf6e799821215f49a7a8aef05974d6f
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-14 13:10:03 -0400

    set up components and stories for interviewer list and list item

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,6 @@
+return (
+  <section className="interviewers">
+    <h4 className="interviewers__header text--light">Interviewer</h4>
+    <ul className="interviewers__list"></ul>
+  </section>
+);
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,10 @@
+return (
+  <li className="interviewers__item">
+    <img
+      className="interviewers__item-image"
+      src="https://i.imgur.com/LpaY82x.png"
+      alt="Sylvia Palmer"
+    />
+    Sylvia Palmer
+  </li>
+);
