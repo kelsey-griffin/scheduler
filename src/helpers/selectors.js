1c2798f0840c76fa4fada069ce087c1cab8cdf11
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-15 17:03:45 -0400

    set up helpers folder with tests, first helper get appt for day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,11 @@
+//returns an array of appointments when given a day
+export function getAppointmentsForDay(state, day) {
+  const apptForDay = [];
+  const dayObj = state.days.filter(i => i.name === day);
+  if (dayObj.length) {
+    dayObj[0].appointments.forEach(elem => {
+      apptForDay.push(state.appointments[elem.toString()]);
+    })
+  }
+  return apptForDay;
+}
