4000261f2e5791ae054a2dfe0c226008e371d3dc
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-16 15:12:16 -0400

    build custom hook for switching visual mode, and tests

---
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -0,0 +1,62 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+
+import useVisualMode from "hooks/useVisualMode";
+
+
+const FIRST = "FIRST";
+const SECOND = "SECOND";
+const THIRD = "THIRD";
+
+//INITIALIZE
+test("useVisualMode should initialize with default value", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  expect(result.current.mode).toBe(FIRST);
+});
+
+//TRANSITION FUNCTION
+test("useVisualMode should transition to another mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+});
+
+//BACK FUNCTION
+test("useVisualMode should return to previous mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+//BACK LIMIT
+test("useVisualMode should not return to previous mode if already at initial", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
+
+//TRANSITION WITH REPLACE 
+test("useVisualMode should replace the current mode", () => {
+  const { result } = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD, true));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+});
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function useVisualMode(initialMode) {
+  const [ history, setHistory ] = useState([initialMode])
+  function transition(mode, replace = false) {
+    replace ? 
+    setHistory(prev => [...prev.slice(0, prev.length-1), mode])
+    : setHistory(prev => [...prev, mode])
+  };
+  function back() {
+    history.length > 1 && setHistory(prev => prev.slice(0, prev.length-1))
+  }
+  return {mode: history[history.length-1], transition, back}
+}
