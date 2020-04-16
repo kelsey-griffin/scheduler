9011e8846ef3b8195198f5bc55a512e0e1890385
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-16 17:39:12 -0400

    import getInterviewersForDay and pass into Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,7 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
-import { getAppointmentsForDay, getInterview } from "helpers/selectors"
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors"
 
 export default function Application(props) {
 
@@ -19,6 +19,7 @@ export default function Application(props) {
 
   const setDay = day => setState({ ...state, day });
   const appointments = getAppointmentsForDay(state, state.day);
+  const interviewers = getInterviewersForDay(state, state.day);
 
   const listOfAppointments = appointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
@@ -29,6 +30,7 @@ export default function Application(props) {
         id={appointment.id}
         time={appointment.time}
         interview={interview}
+        interviewers={interviewers}
       />
     );
   });
@@ -57,12 +59,6 @@ export default function Application(props) {
         console.log(all)
       })      
   }, [])
-
-  // const listOfAppointments = getAppointmentsForDay(state, state.day).map(appointment => {
-  //   return (
-  //     <Appointment key={appointment.id} {...appointment}/>
-  //   )
-  // })
 
   return (
     <main className="layout">
