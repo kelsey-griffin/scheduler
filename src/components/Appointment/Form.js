95059c1a95d651300c0597d17b3bd8f11b3937df
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-17 19:05:06 -0400

    add catch so that empty forms may not be submitted

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -42,7 +42,9 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => props.onSave(name, interviewer)} confirm>Save</Button>
+          <Button onClick={() => {
+            if (name && interviewer) props.onSave(name, interviewer)
+            }} confirm>Save</Button>
         </section>
       </section>
     </main>
