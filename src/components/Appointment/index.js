146f99ba1ad1cad692e906cf6e2d457b93f99514
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-17 19:41:10 -0400

    implement EDIT mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -16,6 +16,7 @@ const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
 const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -49,6 +50,7 @@ export default function Appointment(props) {
             student={props.interview.student}
             interviewer={props.interview.interviewer}
             onDelete={() => transition(CONFIRM)}
+            onEdit={() => transition(EDIT)}
           />
         )}
         {mode === CREATE && (
@@ -73,6 +75,15 @@ export default function Appointment(props) {
             onConfirm={deleteAppointment}
             onCancel={() => back(SHOW)}
             message="Are you sure..."
+          />
+        )}
+        {mode === EDIT && (
+          <Form
+            name={props.interview.student}
+            interviewers={props.interviewers}
+            interviewer={props.interview.interviewer.id}
+            onCancel={() => back(SHOW)}
+            onSave={save}
           />
         )}
     </article>
