66d901094e0e5d13b3dcd6c489654204decab08b
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-18 00:27:04 -0400

    get number of spots with countSpots, not yet updating state

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -6,8 +6,41 @@ export default function useApplicationData(/*???*/) {
     day: "Monday",
     days: [],
     appointments: {},
-    interviewers: {}
+    interviewers: {},
+    spots: 5
   });
+  
+  console.log("days: ", state.days)
+  
+  function countSpots() {
+    const copy = {...state};
+    console.log("copy --> ", copy)
+
+    const specificDay = copy.days.filter(day => day.name === copy.day)
+    console.log("spec day: ", specificDay)
+
+    const dayID = specificDay[0] && specificDay[0].id
+    console.log("day id ", dayID)
+
+    if (copy.appointments && specificDay[0]) { 
+      console.log("specific day: ", specificDay[0].appointments)
+      
+      let counter = 0;
+      specificDay[0].appointments.forEach(elem => {
+        console.log("elem --> ", copy.appointments[elem].interview)
+        if (copy.appointments[elem].interview === null) counter ++;
+      })
+      console.log(counter);
+      const target = state.days[dayID - 1]
+      const days = {
+        ...state.days, [dayID - 1]: {...target, spots: counter}
+      }
+      setState({...state, days})
+    }
+  }
+  countSpots();
+  // const setSpots = spots => setState({ ...state, spots });
+  // setSpots(countSpots());
 
   function cancelInterview(id) {
     const target = state.appointments[id]
@@ -21,7 +54,6 @@ export default function useApplicationData(/*???*/) {
     })
     .then(result => {
       setState({...state, appointments})
-      console.log(result)
     });
   }
 
@@ -43,7 +75,6 @@ export default function useApplicationData(/*???*/) {
     })
     .then(result => {
       setState({...state, appointments})
-      console.log(result)
     });
   }
 
@@ -57,16 +88,16 @@ export default function useApplicationData(/*???*/) {
         axios.get('/api/days')
           .then(res => res.data)
           .catch(error => error)),
-        Promise.resolve(
-          //get appointments data from api
-          axios.get('/api/appointments')
-            .then(res => res.data)
-            .catch(error => error)),
-        Promise.resolve(
-          //get appointments data from api
-          axios.get('/api/interviewers')
-            .then(res => res.data)
-            .catch(error => error))
+      Promise.resolve(
+        //get appointments data from api
+        axios.get('/api/appointments')
+          .then(res => res.data)
+          .catch(error => error)),
+      Promise.resolve(
+        //get appointments data from api
+        axios.get('/api/interviewers')
+          .then(res => res.data)
+          .catch(error => error))
       ])
       .then(all => {
         setState(prev => ({ ...prev, days: all[0], appointments: all[1], interviewers: all[2]}));
