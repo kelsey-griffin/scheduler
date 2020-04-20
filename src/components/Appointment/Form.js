ca240a59058806829d895fcb4ac1cdeca8891b93
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-20 17:53:57 -0400

    add function to validate form error and track error state

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,7 +5,8 @@ import InterviewerList from "components/InterviewerList"
 export default function Form(props) {
   const [ name, setName ] = useState(props.name || "");
   const [ interviewer, setInterviewer ] = useState(props.interviewer || null); 
-  
+  const [error, setError] = useState("");
+
   const handleNameInput = (evt) => {
     setName(evt.target.value)
   }
@@ -20,6 +21,14 @@ export default function Form(props) {
     props.onCancel();
     reset();
   }
+  const validate = () => {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
+  }  
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -34,6 +43,7 @@ export default function Form(props) {
             data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers={props.interviewers} 
           value={interviewer} 
@@ -43,9 +53,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={() => {
-            props.onSave(name, interviewer)
-            }} confirm>Save</Button>
+          <Button onClick={() => validate() } confirm>Save</Button>
         </section>
       </section>
     </main>
