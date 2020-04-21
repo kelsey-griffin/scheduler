5b55647dea4905e145caff633391ad8ace6739bb
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-21 17:12:49 -0400

    complete test and remove console.log/debug

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { render, cleanup, waitForElement, fireEvent, prettyDOM} from "@testing-library/react";
-import { getByText, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
+import { getByText, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 import "__mocks__/axios";
 import Application from "components/Application";
 
@@ -36,10 +36,13 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     //click save button
     fireEvent.click(getByText(appointment, "Save"));
-    debug();
     expect(getByText(appointment, "Saving...")).toBeInTheDocument();
 
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
-
+    
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });  
 })
