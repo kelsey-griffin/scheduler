c349fc31ff868817d3cda8cc7b05fe4416f2f5d1
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-21 16:11:35 -0400

    add test for booking a new interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
-
+import { render, cleanup, waitForElement, fireEvent, prettyDOM} from "@testing-library/react";
+import { getByText, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 import "__mocks__/axios";
 import Application from "components/Application";
 
@@ -15,5 +15,28 @@ describe("Appointment", () => {
     fireEvent.click(getByText("Tuesday"));
     
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  })  
+  });
 });
+
+describe("Application", () => {
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+    
+    //click the add button
+    fireEvent.click(getByAltText(appointment, "Add"));
+    //change the name
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });    
+    //click new interviewer
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    //click save button
+    fireEvent.click(getByText(appointment, "Save"));
+
+    console.log(prettyDOM(appointment))
+  });  
+})
