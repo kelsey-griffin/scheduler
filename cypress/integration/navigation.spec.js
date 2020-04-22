69a25912a105e84a2cb7b95c7e3eb9799b255f08
Kelsey Griffin <kjulegriffin@gmail.com>
2020-04-22 18:06:45 -0400

    minor fix

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,6 +2,7 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+  
   it("should navigate to Tuesday", () => {
     cy.visit("/");
     cy.contains("[data-testid=day]", 'Tuesday')
